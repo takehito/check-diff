428e2fe882ba4d0a74609ebc5cc03a02e2d0a1c1
takehito <fukuda_kento@fukehito.com>
2022-10-13 04:29:32 +0900

    戻り値の修正
    空白の要素を返すことがあったため

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
-import {wait} from './wait'
 
 async function getDiff(baseRef: string, path: string[]): Promise<string[]> {
   let myOutput = '';
@@ -26,13 +25,11 @@ async function getDiff(baseRef: string, path: string[]): Promise<string[]> {
   ]
 
   if (path.length > 0) {
-    core.debug(`${path.length}`)
     args.push(
       '--',
       ...path,
     )
   }
-  core.debug(`${args}`)
 
   await exec.getExecOutput(
     '/usr/bin/git',
@@ -40,14 +37,11 @@ async function getDiff(baseRef: string, path: string[]): Promise<string[]> {
     options,
   )
 
-  return myOutput.toString().split("\n")
+  return myOutput.toString().trim().split("\n")
 }
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
     const br = process.env.GITHUB_BASE_REF?.toString()
     if (br === undefined) {
       throw new Error("set GITHUB_BASE_REF");
@@ -57,9 +51,7 @@ async function run(): Promise<void> {
       trimWhitespace: true,
     });
     const files = await getDiff(br, paths)
-    files.forEach(v => {
-      core.debug(v)
-    })
+    core.debug(`${files.length}`)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
